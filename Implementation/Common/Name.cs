6560174993eeeb07f9ee87c72e48a60022ec76ef
Eset35 <37243389+Eset35@users.noreply.github.com>
2019-03-21 22:19:02 +0300

    Add files via upload

--- Implementation/Common/Name.cs
+++ Implementation/Common/Name.cs
@@ -3,7 +3,7 @@
     /// <summary>
     ///     Скрытая реализация представления об имени человека.
     /// </summary>
-    internal struct Name
+    internal struct Name : IName
     {
         /*
          * TODO #1: Реализуйте интерфейс IName для структуры Name
@@ -12,17 +12,20 @@
         /// <summary>
         ///     Имя.
         /// </summary>
-        public string FirstName { get; }
-        
+        public string FirstName { get; set; }
+
         /// <summary>
         ///     Фамилия.
         /// </summary>
-        public string Surname { get; }
-        
+        public string Surname { get; set;  }
+
         /// <summary>
         ///     Отчество.
         /// </summary>
-        public string Patronymic { get; }
-        
+        public string Patronymic { get; set; }
+
+        public string FullName => Surname + ' ' + FirstName + ' ' + Patronymic;
+
+        public string ShortName => Surname + ' ' + FirstName[0] + ". " + Patronymic[0]+".";
     }
 }
--- Implementation/HR/Employee.cs
+++ Implementation/HR/Employee.cs
@@ -1,12 +1,27 @@
-﻿namespace Practice.HR
+﻿using System;
+using Practice.HR.Events;
+using Practice.Organization;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Скрытая реализация представления о сотруднике.
     /// </summary>
-    internal class Employee : AbstractPerson
+    internal class Employee : AbstractPerson, IEmployee
     {
         /*
          * TODO #5: Реализуйте интерфейс IEmployee для класса Employee
          */
+        private IDepartment _department;
+        public IDepartment Department
+        { get => _department;
+            set
+            {
+                DepartmentChange?.Invoke(this, new ValueChangeEventArgs<IDepartment>(_department));
+                _department = value;
+            }
+        }
+
+        public event EventHandler<ValueChangeEventArgs<IDepartment>> DepartmentChange;
     }
 }
